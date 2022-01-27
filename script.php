c46514453e3fb17420b41dfc5a6888d869c9353e
Elias Hollstrand <elias.hollstrand@icloud.com>
2022-01-27 10:59:42 +0100

    Added files

--- script.php
+++ script.php
@@ -2,18 +2,51 @@
 session_start();
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html>
 <body>
-    
-<?php
 
+<?php   
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "forum-for-farskallar";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}   
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+$login_success = false;
+$name = "";
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+		if($row["username"] == $_POST["username"] && $row["password"] == $_POST["password"]) {
+			$login_success = true;
+			$name = $row["username"];
+            $_SESSION["user"] = $name;
+            
+	    }
+    } 
+} else {
+    echo "0 results";
+}
+
+if($login_success) {
+    echo "<h1>Forum för fårskallar</h1>";
+    echo "Welcome " . $name . "!<br><br>";
+    echo "<button style='margin-bottom: 50px;'>Skapa tråd</button><br>"; 
+    echo "Det finns X trådar:";
+
+
+} else {
+    echo "Incorrect login credentials";
+}
+$conn->close();
 ?>
 
-</body>
 </html>
+</body>
