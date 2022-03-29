8b3dbca2a789efe33d57040710ad2bf7e44c017d
Elias Hollstrand <elias.hollstrand@icloud.com>
2022-03-29 13:11:52 +0200

    Funktion för att gilla inlägg med AJAX

--- comment.php
+++ comment.php
@@ -54,8 +54,8 @@
                     $mail->isSMTP();                                           
                     $mail->Host       = 'smtp.gmail.com';                     
                     $mail->SMTPAuth   = true;                               
-                    $mail->Username   = 'forumforfarskallar@gmail.com';                  
-                    $mail->Password   = 'farskalle123';                   
+                    $mail->Username   = '';                  
+                    $mail->Password   = '';                   
                     $mail->SMTPSecure = "tls";            
 
                     $mail->setFrom('forumforfarskallar@gmail.com', 'Forum för fårskallar');
